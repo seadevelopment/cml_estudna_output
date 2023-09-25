a51c5f4b9288e7e87d045dddbca72554b12933b0
seadevelopment <105720418+seadevelopment@users.noreply.github.com>
2023-09-25 13:26:56 +0200

    Create estudna.php

---
+++ estudna.php
@@ -0,0 +1,189 @@
+<?php
+
+/**
+ * CML - How to get value of water level for you eStudna device
+ * 
+ * 2022-05-17 v1-01
+ * a) Project foundation
+ */
+
+// ----------------------------------------------------------------------------
+// --- Configuration
+// ----------------------------------------------------------------------------
+
+// --- Configuration
+$user = 'zakaznik@email.com';
+$password = 'heslo';
+$sn = 'SB819098';   // Serial number of your eSTUDNA
+
+
+
+// ----------------------------------------------------------------------------
+// --- Code
+// ----------------------------------------------------------------------------
+
+function httpPost($url,$data,$header)
+{
+    
+    // use key 'http' even if you send the request to https://...
+    $options = array
+    (
+        'http' => array
+        (
+            'header'  => 'Content-Type: application/json\r\n' . $header,
+            'method'  => 'POST',
+            'content' => json_encode($data)
+        )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    if ($result === FALSE)
+    {
+        throw new Exception('HTTP request failed!');
+    }
+
+    return $result;
+}
+
+function httpGet($url,$header)
+{
+    
+    // use key 'http' even if you send the request to https://...
+    $options = array
+    (
+        'http' => array
+        (
+            'header'  => $header,
+            'method'  => 'GET',
+        )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    if ($result === FALSE)
+    {
+        throw new Exception('HTTP request failed!');
+    }
+
+    return $result;
+}
+
+/**
+ * Objekt pro pristup k Thingsboardu
+ */
+class ThingsBoard
+{
+    public $server =  'https://cml.seapraha.cz';
+    public $userToken = null;
+    public $customerId = null;
+
+    /**
+     * Prihlaseni uzivatele
+     */
+    public function login($user,$password)
+    {
+        // Login
+        $url = $this->server . '/api/auth/login';
+        $result = httpPost($url,array('username' => $user, 'password' => $password),"");
+
+        $response = json_decode($result);
+        $this->userToken = $response->token;    // User token
+
+        // Get customer ID
+        $url = $this->server . '/api/auth/user';
+        $result = httpGet($url,'X-Authorization: Bearer ' . $this->userToken );
+        
+        $response = json_decode($result);
+        $this->customerId = $response->customerId->id; // Customer ID
+            
+    }
+
+    /**
+     * Vyhledani zarizeni podle nazvu
+     */
+    public function getDevicesByName($name)
+    {
+        $url = $this->server . '/api/customer/' . $this->customerId . '/devices?pageSize=100&page=0&&textSearch='.urlencode($name);
+        
+        $result = httpGet($url, 'X-Authorization: Bearer ' . $this->userToken);
+        $response = json_decode($result);
+        if ($response->totalElements < 1)
+            throw new Exception('Device SN ' . $name . ' has not been found!');
+
+        return $response->data;         // Return list of devices
+    }
+
+    /**
+     * Cteni aktualnich hodnot ze zarizeni
+     */
+    public function getDeviceValues($deviceId, $keys)
+    {
+        $url = $this->server . '/api/plugins/telemetry/DEVICE/' . $deviceId .'/values/timeseries?keys=' . $keys;
+        
+        $result = httpGet($url,'X-Authorization: Bearer ' . $this->userToken);
+        $response = json_decode($result);
+  
+        return $response;
+    }
+
+    public function setDeviceOutput($deviceId, $output, $value)
+    {
+        if ($output == 'OUT1') 
+        {
+            $output = 'setDout1';
+        } else
+        {
+            $output = 'setDout2';
+        }
+        $data = array(
+            "method" => $output,
+            "params" => $value
+        );
+        $url = $this->server . '/api/rpc/twoway/' . $deviceId;
+        
+        httpPost($url, $data, 'X-Authorization: Bearer ' . $this->userToken);
+    }
+}
+
+/**
+ * Cteni hladiny ve studni
+ */
+function eStudna_GetWaterLevel($user,$password,$serialNumber)
+{
+    $tb = new ThingsBoard();
+    $tb->login($user,$password);
+    $devices = $tb->getDevicesByName( '%' . $serialNumber);
+    $values = $tb->getDeviceValues($devices[0]->id->id,'ain1');
+    return $values->ain1[0]->value;  
+}
+
+function eStudna_SetOutput($user, $password, $serialNumber, $output, $value)
+{
+    $tb = new ThingsBoard();
+    $tb->login($user,$password);
+    $devices = $tb->getDevicesByName( '%' . $serialNumber);
+    $tb->setDeviceOutput($devices[0]->id->id, $output, $value);
+}
+
+
+// ----------------------------------------------------------------------------
+// --- Main code
+// ----------------------------------------------------------------------------
+
+try
+{
+  $level = eStudna_GetWaterLevel($user, $password, $sn);
+  echo('<pre><b>' . $sn . '</b></pre>');
+  echo('<pre>WATER LEVEL [m]: '.$level.'</pre>');
+  echo('<pre>OUT1: true</pre>');
+  eStudna_SetOutput($user, $password, $sn, "OUT1", False);
+  echo('<pre>OUT1: false</pre>');
+  sleep(1);
+  eStudna_SetOutput($user, $password, $sn, "OUT1", True);
+  echo('<pre>OUT1: true</pre>');
+} catch (Exception $e) {
+    echo('<pre>[ EXCEPTION] - ' . $e->getMessage() . '</pre>');
+}
+
+?>
